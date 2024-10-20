c19424781d9941aed4816ac74640f5b6f6898fb8
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-10-20 13:28:52 +0600

    Create an restaurant and manu class

--- Restaurant Management System/user.py
+++ Restaurant Management System/user.py
@@ -20,17 +20,47 @@ class Employee(User):
 class Admin(User):
     def __init__(self, name, phone, email, address):
         super().__init__(name, phone, email, address)
-        self.employee = [] #This is our database
 
-    def add_employee(self, name, phone, email, address, age, designation, salary):
-        employee = Employee(name, phone, email, address, age, designation, salary) #create an object of Employee class
-        self.employee.append(employee)
-        print(f'{name} is added!')
+    def add_employee(self, employee, restaurant):
+        restaurant.add_employee(employee)
+        
+    def view_employee(self, restaurant):
+       restaurant.view_employee()
+
+class Restaurant:
+    def __init__(self, name):
+        self.name = name
+        self.employee = [] #This is our database
     
+    def add_employee(self, employee):
+        self.employee.append(employee)
+
     def view_employee(self):
         print('Employee List : ')
         for emp in self.employee:
             print(emp.name, emp.phone, emp.email, emp.address, emp.age, emp.designation, emp.salary)
+
+class Manu :
+    def __init__(self):
+        self.items = [] #database of an items
+    
+    def add_manu_item(self, item):
+        self.items.append(item)
+
+    def find_item(self, item_name):
+        for item in self.items:
+            if item.name.lower() ==  item_name.lower():
+                return item
+            return None
+        
+    def remove_item(self, item_name):
+        item = self.find_item(item_name)
+        if item:
+            self.items.remove(item)
+            print(f'This {item_name} is deleted!!')
+        else:
+            print(f'Item not found')
+        
 
 ad = Admin("Shuvo", "01793867764", "rakibulhasanshuvo206@gmail.com", "Mipur-13")
 ad.add_employee("Rakibul", "01793864323", "rakibulhasanshuvo1263@gmail.com", "Mipur-2", 20, "Engineer", 10000)
